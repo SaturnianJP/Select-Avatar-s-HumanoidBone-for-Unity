55ec5692d15a8adbbcaf10281b63634219855a72
さたにあ <126487136+SaturnianJP@users.noreply.github.com>
2023-06-09 18:15:08 +0900

    1.0.0

---
+++ BoneSelect.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class BoneSelect : MonoBehaviour
+{
+    [MenuItem("GameObject/Select HumanoidBone", false, 0)]
+    static void Init()
+    {
+        GameObject Avatar = Selection.activeGameObject;
+        if (Avatar == null)
+            return;
+
+        Animator _Animator = Avatar.GetComponent<Animator>();
+        if (_Animator == null)
+            return;
+
+        Transform[] Bones = new Transform[(int)HumanBodyBones.LastBone];
+        for (int i = 0; i < (int)HumanBodyBones.LastBone; i++)
+        {
+            Transform bone = _Animator.GetBoneTransform((HumanBodyBones)i);
+            if (bone == null)
+                continue;
+
+            Bones[i] = bone;
+        }
+
+        List<GameObject> SelectGameObjects = new List<GameObject>();
+        foreach (Transform bn in Bones)
+        {
+            if (bn == null)
+                continue;
+
+            SelectGameObjects.Add(bn.gameObject);
+        }
+
+        if (SelectGameObjects.Count > 0)
+            Selection.objects = SelectGameObjects.ToArray();
+
+    }
+}
